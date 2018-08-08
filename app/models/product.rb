2fa556a52f06a3406a374d321b2a684f7dbd477b
Moacir Petry <m.petry9@gmail.com>
2018-08-08 20:16:18 -0300

    adjust validate in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category_product
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, :description, :category_product_id, presence: true
+
+  validates :name, length: { maximum: 100, too_long: "-> %{count} characters is the maximum allowed", minimum: 2, too_short: "-> %{count} characters is the minimum allowed" }
+  validates :description, length: { maximum: 100, too_long: "-> %{count} characters is the maximum a" }
+
 end
