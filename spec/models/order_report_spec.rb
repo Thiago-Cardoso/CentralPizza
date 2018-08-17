6ea4b52c824ac33c4680845e800a79fffc97d890
Moacir Petry <m.petry9@gmail.com>
2018-08-17 00:24:35 -0300

    order_report teste and validates implemented

--- spec/models/order_report_spec.rb
+++ spec/models/order_report_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe OrderReport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :order_report
+  context "Unit tests for Order Report Model!!" do
+    before(:each) do
+      @order_report = order_report(:order_report001)
+    end
+
+    it "Valid order report?" do
+  	  expect(@order_report.valid?).to be_truthy
+  	end
+
+    it "Valid order report with null description?" do
+      @order_report.description = nil
+  	  expect(@order_report.valid?).to be_falsey
+  	end
+
+    it "Valid order report with null date time?" do
+      @order_report.date_time = nil
+  	  expect(@order_report.valid?).to be_falsey
+  	end
+
+    it "Valid order report with null order id?" do
+      @order_report.order_id = nil
+  	  expect(@order_report.valid?).to be_falsey
+  	end
+
+    it "Valid order report with null user id?" do
+      @order_report.user_id = nil
+  	  expect(@order_report.valid?).to be_falsey
+  	end
+  end
 end
