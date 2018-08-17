dec5a743ebeec9ab841de9dcd24fbdb6254c3ef2
Moacir Petry <m.petry9@gmail.com>
2018-08-17 00:03:26 -0300

    order_item test and validates implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814025859) do
+ActiveRecord::Schema.define(version: 20180814030351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,26 @@ ActiveRecord::Schema.define(version: 20180814025859) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+    t.index ["product_id"], name: "index_order_items_on_product_id", using: :btree
+  end
+
+  create_table "order_reports", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "date_time"
+    t.integer  "order_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["order_id"], name: "index_order_reports_on_order_id", using: :btree
+    t.index ["user_id"], name: "index_order_reports_on_user_id", using: :btree
   end
 
   create_table "orders", force: :cascade do |t|
@@ -86,6 +106,10 @@ ActiveRecord::Schema.define(version: 20180814025859) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "order_reports", "orders"
+  add_foreign_key "order_reports", "users"
   add_foreign_key "orders", "tables"
   add_foreign_key "payments", "orders"
   add_foreign_key "products", "category_products"
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :order_item
+  context "Unit tests for Order Item Model!!" do
+    before(:each) do
+      @order_item = order_item(:order_item001)
+    end
+
+    it "Valid order item?" do
+  	  expect(@order_item.valid?).to be_truthy
+  	end
+
+    it "Valid order item with null order id?" do
+      @order_item.order_id = nil
+  	  expect(@order_item.valid?).to be_falsey
+  	end
+
+    it "Valid order with null product id?" do
+      @order_item.product_id = nil
+  	  expect(@order_item.valid?).to be_falsey
+  	end
+  end
 end
